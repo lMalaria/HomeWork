d1ffa4c537d6a205f23d7e4bd0bcc687a75f765a
lMalaria <shinobi441@gmail.com>
2018-09-16 23:57:08 +0900

    Done
    done

---
+++ FirstHomeWork/Assets/Scripts/Find/Check.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+public class Check : MonoBehaviour {
+
+    [SerializeField]
+    private float FindingRadius;
+
+    [SerializeField]
+    private LayerMask findingRayers;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, FindingRadius);
+    }
+
+    void Update ()
+    {
+        Collider[] Colliders = Physics.OverlapSphere(transform.position, FindingRadius, findingRayers);
+        Array.Sort(Colliders, new DistanceComparer(transform));
+
+        foreach (Collider item in Colliders)
+        {
+            Debug.Log(item.name);
+        }
+
+    }
+}
---
+++ FirstHomeWork/Assets/Scripts/Find/DistanceComparer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System;
+using UnityEngine;
+
+public class DistanceComparer : IComparer {
+
+    private Transform compareTransform;
+
+    public DistanceComparer(Transform compTransform)
+    {
+        compareTransform = compTransform;
+    }
+
+    public int Compare(object a, object b)
+    {
+        Collider aCollider = a as Collider;
+        Collider bCollider = b as Collider;
+
+        Vector3 Offset = aCollider.transform.position - compareTransform.position;
+        float aDistance = Offset.sqrMagnitude; 
+
+        Offset = bCollider.transform.position - compareTransform.position;
+        float bDistance = Offset.sqrMagnitude;
+
+        return aDistance.CompareTo(bDistance);
+    }
+}
